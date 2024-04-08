b486dd7c60ca043687034d8f172964f7ab053331
Mudar <mudarshullar22@gmail.com>
2024-04-08 17:22:24 +0200

    missing function

--- SmartSystem/dataGeneration.py
+++ SmartSystem/dataGeneration.py
@@ -1,5 +1,8 @@
 import psycopg2
 import joblib
+import random
+import logging
+from datetime import datetime
 from SmartSystem.config import load_database_config
 from SmartSystem.databaseManagement import connect_to_database
 
@@ -29,4 +32,22 @@ def get_latest_sensor_data():
 
     except psycopg2.Error as e:
         print("Fehler bei der Verbindung zur PostgreSQL-Datenbank:", e)
+        return None
+
+
+def generate_sensor_data():
+    """
+    Generiert zufällige Sensordaten.
+    :return: Tuple mit Zeitstempel, Temperatur, Luftfeuchtigkeit, CO2-Werte und TVOC-Werte
+    """
+    try:
+        temperature = round(random.uniform(10, 27), 2)
+        humidity = round(random.uniform(30, 62), 2)
+        co2_values = round(random.uniform(402, 600), 2)
+        tvoc_values = round(random.uniform(100, 380), 2)
+        timestamp = datetime.now()
+
+        return timestamp, temperature, humidity, co2_values, tvoc_values
+    except Exception as e:
+        logging.error("Fehler beim Generieren von Sensordaten: %s", str(e))
         return None
