3545c57654219efdb13b781c3c5d78321d6708aa
Mudar <mudarshullar22@gmail.com>
2024-09-04 15:53:08 +0200

    mqqt topics wurden geändert

--- smart_ventilation/mqtt_client.py
+++ smart_ventilation/mqtt_client.py
@@ -87,9 +87,11 @@ class MQTTClient:
             self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/24e124707c481005/event/up")
 
             # Für Uhrzeit, Co2, Luftfeuchtigkeit, Temperaturen
-            self.client.subscribe("application/cefebad2-a2a8-49dd-a736-747453fedc6c/device/0004a30b00fd0f5e/event/up")
+            #self.client.subscribe("application/cefebad2-a2a8-49dd-a736-747453fedc6c/device/0004a30b00fd0f5e/event/up")
+            self.client.subscribe("application/cefebad2-a2a8-49dd-a736-747453fedc6c/device/0004a30b00fd09aa/event/up")
 
             # Für Außentemperaturen
+            #self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa92/event/up")
             self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa92/event/up")
 
             if not self.prediction_thread.is_alive():
