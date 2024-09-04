78f91c683ecf65993668d7639d77f9bd3297810a
Mudar <mudarshullar22@gmail.com>
2024-09-04 17:18:17 +0200

    mqqt wieder angepasst

--- smart_ventilation/mqtt_client.py
+++ smart_ventilation/mqtt_client.py
@@ -84,19 +84,20 @@ class MQTTClient:
             logging.info("Verbunden mit Ergebniscode" + str(rc))
 
             # Für Uhrzeit und TVOC
-            self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/24e124707c481005/event/up")
+            self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/24e124707c489656/event/up")
 
             # Für Uhrzeit, Co2, Luftfeuchtigkeit, Temperaturen 
             # (auskommentiert kommt vom Besprechungsraum)
             # self.client.subscribe("application/cefebad2-a2a8-49dd-a736-747453fedc6c/device/0004a30b00fd0f5e/event/up")
-            #self.client.subscribe("application/cefebad2-a2a8-49dd-a736-747453fedc6c/device/0004a30b00fd09aa/event/up")
-            self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/24e124707c481005/event/up")
+            # self.client.subscribe("application/cefebad2-a2a8-49dd-a736-747453fedc6c/device/0004a30b00fd09aa/event/up")
+            # self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/24e124707c481005/event/up")
+            self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/0004a30b010404f3/event/up")
 
             # Für Außentemperaturen 
             # (auskommentiert kommt vom Besprechungsraum)
             # self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa92/event/up")
-            #self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa92/event/up")
-            self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa58/event/up")
+            self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa92/event/up")
+            # self.client.subscribe("application/f4994b60-cc34-4cb5-b77c-dc9a5f9de541/device/647fda000000aa58/event/up")
 
             if not self.prediction_thread.is_alive():
                 logging.warning("Der Thread wurde angehalten und wird neu gestartet...")
@@ -167,7 +168,7 @@ class MQTTClient:
                 if tvoc_value is not None:
                     self.combined_data.setdefault("tvoc", []).append(round(tvoc_value, 2))
 
-            elif topic.endswith("647fda000000aa58/event/up"):
+            elif topic.endswith("fda000000aa92/event/up"):
                 ambient_temp_value = payload["object"].get("ambient_temp")
 
                 if ambient_temp_value is not None:
