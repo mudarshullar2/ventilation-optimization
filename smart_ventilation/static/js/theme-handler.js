ad21d3089aa5f4d6bd056cd9957254b2148d6a0f
Mudar <mudarshullar22@gmail.com>
2024-09-26 07:38:37 +0200

    redundant comments

--- smart_ventilation/static/js/theme-handler.js
+++ smart_ventilation/static/js/theme-handler.js
@@ -1,20 +1,16 @@
-// Funktion zum Öffnen des Modals
 function openDesignModal() {
     document.getElementById('designModal').style.display = 'flex';
 }
 
-// Funktion zum Schließen des Modals
 function closeDesignModal() {
     document.getElementById('designModal').style.display = 'none';
 }
 
-// Funktion zum Festlegen eines Themas
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
-// Funktion zum Laden des Themas beim Start der Seite
 (function () {
     const savedTheme = localStorage.getItem('theme') || 'theme-light'; // Standardmäßig hell 
     setTheme(savedTheme);
